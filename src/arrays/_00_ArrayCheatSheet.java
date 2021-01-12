32329a80e04fb86bf6f2f1bd03ebb1979b05ed89
bbmma <bbmma@192.168.0.24>
2021-01-11 17:01:11 -0800

    .

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,27 +1,51 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+		String[] string = {"text1","text2","text3","text4","text5"};
 		//2. print the third element in the array
-		
+		System.out.println(string[2]);
 		//3. set the third element to a different value
-		
+		string[2]="newtext";
 		//4. print the third element again
-		
+		System.out.println(string[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
-		
+		for (int i = 0; i<string.length;i++) {
+			System.out.println(string[i]);
+		}
 		//6. make an array of 50 integers
-		
+		int[] integerList = new int[50];
+		for (int i = 0; i<integerList.length;i++) {
+			integerList[i]=i+1;
+		}
 		//7. use a for loop to make every value of the integer array a random number
-		
+		Random ran = new Random();
+		for (int i = 0; i<integerList.length;i++) {
+			integerList[i]=ran.nextInt(50);
+		}
 		//8. without printing the entire array, print only the smallest number in the array
-		
+		int smallest = integerList[0];
+		for (int i = 1; i<integerList.length;i++){
+			if (integerList[i]<smallest) {
+				smallest = integerList[i];
+			}
+		}
+		System.out.println(smallest);
 		//9 print the entire array to see if step 8 was correct
-		
+		for (int i = 0; i<integerList.length;i++) {
+			System.out.println(integerList[i]);
+		}
 		//10. print the largest number in the array.
+		int largest = integerList[0];
+		for (int i = 1; i<integerList.length;i++){
+			if (integerList[i]>largest) {
+				largest = integerList[i];
+			}
+		}
+		System.out.println(largest);
 	}
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,22 +1,68 @@
 package arrays;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
-	//1. make a main method
+	// 1. make a main method
+	public static void main(String[] args) {
+		Robot[] rob = new Robot[5];
+		int[] turnNum = {0,0,0,0,0};
+		for (int i = 0; i < rob.length; i++) {
+			rob[i] = new Robot();
+			rob[i].setSpeed(10);
+			rob[i].miniaturize();
+			
+		}
+		for (int i = 0; i < rob.length; i++) {
+			rob[i].turn(90);
+			rob[i].setY(80+20*i);
+			rob[i].setX(300);
+		}
 
-	//2. create an array of 5 robots.
+		Random ran = new Random();
 
-	//3. use a for loop to initialize the robots.
+		boolean isFinished = false;
+		for (;;) {
+			for (int i = 0; i < rob.length; i++) {
+				int num = ran.nextInt(3);
+				for (int j=0;j<num;j++) {
+				rob[i].move(ran.nextInt(3)+1);
+				rob[i].turn(1);				
+				}
+				turnNum[i]+=num;
+				if (turnNum[i]>360) {
+					isFinished = true;
+					if (isFinished) {
+						JOptionPane.showMessageDialog(null, rob[i]+" has won!!");
+						break;
+					}
+				}
+			}
+			if (isFinished) {
+				break;
+			}
+		}
 
-		//4. make each robot start at the bottom of the screen, side by side, facing up
-
-	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
-
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
-	//7. declare that robot the winner and throw it a party!
-
-	//8. try different races with different amounts of robots.
-
-	//9. make the robots race around a circular track.
+	}
 }
+// 2. create an array of 5 robots.
+
+// 3. use a for loop to initialize the robots.
+
+// 4. make each robot start at the bottom of the screen, side by side, facing up
+
+// 5. use another for loop to iterate through the array and make each robot move
+// a random amount less than 50.
+
+// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+// screen.
+
+// 7. declare that robot the winner and throw it a party!
+
+// 8. try different races with different amounts of robots.
+
+// 9. make the robots race around a circular track.
